b49f08042a38a4ed49e1e7f81051098e6edd09c2
nadkins00 <nathansadkins@gmail.com>
2025-04-02 20:53:58 -0600

    test commit Please enter the commit message for your changes. Lines starting

--- snake.py
+++ snake.py
@@ -35,7 +35,7 @@ class Snake(pygame.sprite.Sprite):
 
     #def update(self):
         #print("Update Snake")
-        #self.calcnewpos() test
+        #self.calcnewpos() test test2
     
 
 #spawn an apple at a random location 
